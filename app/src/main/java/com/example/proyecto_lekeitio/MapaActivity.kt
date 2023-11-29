e1067e313230b4974b0de45f3544529e347abaf1
Mohamed said El fezzazi <144590770+Med-Said-ELFezzazi@users.noreply.github.com>
2023-11-29 12:14:56 +0100

    ADD pantalla inicio y mapa
    pantalla de inicio finalizada  y mapa con estructura base definida

---
+++ app/src/main/java/com/example/proyecto_lekeitio/MapaActivity.kt
@@ -0,0 +1,37 @@
+package com.example.proyecto_lekeitio
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.view.isVisible
+
+class MapaActivity : AppCompatActivity() {
+
+    lateinit var btnsiguienteActividad: Button
+    val array: Array<String> = arrayOf("actividad1", "actividad2" , "actividad3", "actividad4", "actividad5", "actividad6", "actividad7")
+    lateinit var lblInstruccion : TextView
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mapa)
+
+        var imgMapa : ImageView = findViewById(R.id.imgMapa)
+        var lblTituloMapa : TextView = findViewById(R.id.lblTituloMapa)
+        lblInstruccion = findViewById(R.id.lblInstruccion)
+        btnsiguienteActividad = findViewById(R.id.btnsiguienteActividad)
+        btnsiguienteActividad.isVisible = false
+    }
+
+    fun pasarActividades(view: View) {}
+    fun mostrarDatos(view: View) {
+        lblInstruccion.isVisible = false
+        btnsiguienteActividad.isVisible = true
+
+
+    }
+}
