b59a8ab5859cd915aa72650cf7733b19e297ee17
Joseba <117350714+Josebasmh@users.noreply.github.com>
2023-12-19 10:00:42 +0100

    ADD- FragmentManager y dos Fragmentos

--- app/src/main/java/com/example/proyecto_lekeitio/Act1Activity.kt
+++ app/src/main/java/com/example/proyecto_lekeitio/Act1Activity.kt
@@ -4,15 +4,26 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentContainerView
+import androidx.fragment.app.FragmentManager
 
 class Act1Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_act1)
+
+        val act1Fragment : FragmentContainerView = findViewById(R.id.frActividad1)
+        val fragmentManager : FragmentManager = supportFragmentManager
+
+        fragmentManager.beginTransaction().replace(R.id.frActividad1, EnunciadoAct1Fragment::class.java,null).
+        setReorderingAllowed(true).
+        addToBackStack("Enunciado").
+        commit()
     }
 
     fun volver(view: View) {
-        val resultado = 99
+        val resultado = 1
         setResult(RESULT_OK, Intent().putExtra("RESULTADO", resultado))
         finish()
     }
