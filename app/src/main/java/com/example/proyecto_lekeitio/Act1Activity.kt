bb16a267012b5827bef7cfd6a11a475013e0b216
Joseba <117350714+Josebasmh@users.noreply.github.com>
2023-12-20 12:58:44 +0100

    ADD-fxml
    Añadidas las fotos en el xml

--- app/src/main/java/com/example/proyecto_lekeitio/Act1Activity.kt
+++ app/src/main/java/com/example/proyecto_lekeitio/Act1Activity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentContainerView
 import androidx.fragment.app.FragmentManager
@@ -15,19 +16,10 @@ class Act1Activity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_act1)
 
-        val act1Fragment : FragmentContainerView = findViewById(R.id.frActividad1)
-
         fragmentManager.beginTransaction().replace(R.id.frActividad1, EnunciadoAct1Fragment::class.java,null).
         setReorderingAllowed(true).
         addToBackStack("Enunciado").
         commit()
-    }
-
-
-    fun volver(view: View) {
-        val resultado = 1
-        setResult(RESULT_OK, Intent().putExtra("RESULTADO", resultado))
-        finish()
     }
 
     fun siguiente(view: View) {
--- app/src/main/java/com/example/proyecto_lekeitio/EnunciadoAct1Fragment.kt
+++ app/src/main/java/com/example/proyecto_lekeitio/EnunciadoAct1Fragment.kt
@@ -27,6 +27,7 @@ class EnunciadoAct1Fragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
+
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_enunciado, container, false)
