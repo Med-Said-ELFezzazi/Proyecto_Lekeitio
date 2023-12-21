c0cddf8427f754c89d92e30b7e860180f4831a1b
Joseba <117350714+Josebasmh@users.noreply.github.com>
2023-12-21 09:59:10 +0100

    MOD-Reestructuracion xml e implementación de objetos
    añadidos LinearLayout como espacios donde colocar las imágenes (cada uno con ID) e implementación.

--- app/src/main/java/com/example/proyecto_lekeitio/Act1PuzzleActivity.kt
+++ app/src/main/java/com/example/proyecto_lekeitio/Act1PuzzleActivity.kt
@@ -3,11 +3,58 @@ package com.example.proyecto_lekeitio
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.LinearLayout
 
 class Act1PuzzleActivity : AppCompatActivity() {
+
+    // Declaración de variables
+    private lateinit var btnMapa : Button
+    private lateinit var img1 : ImageView
+    private lateinit var img2 : ImageView
+    private lateinit var img3 : ImageView
+    private lateinit var img4 : ImageView
+    private lateinit var img5 : ImageView
+    private lateinit var img6 : ImageView
+    private lateinit var img7 : ImageView
+    private lateinit var img8 : ImageView
+    private lateinit var img9 : ImageView
+    private lateinit var esp1 : LinearLayout
+    private lateinit var esp2 : LinearLayout
+    private lateinit var esp3 : LinearLayout
+    private lateinit var esp4 : LinearLayout
+    private lateinit var esp5 : LinearLayout
+    private lateinit var esp6 : LinearLayout
+    private lateinit var esp7 : LinearLayout
+    private lateinit var esp8 : LinearLayout
+    private lateinit var esp9 : LinearLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_act1_puzzle)
+
+        // Implementación de variables
+        btnMapa = findViewById(R.id.btnMapa)
+        img1 = findViewById(R.id.img1)
+        img2 = findViewById(R.id.img2)
+        img3 = findViewById(R.id.img3)
+        img4 = findViewById(R.id.img4)
+        img5 = findViewById(R.id.img5)
+        img6 = findViewById(R.id.img6)
+        img7 = findViewById(R.id.img7)
+        img8 = findViewById(R.id.img8)
+        img9 = findViewById(R.id.img9)
+        esp1 = findViewById(R.id.esp1)
+        esp2 = findViewById(R.id.esp2)
+        esp3 = findViewById(R.id.esp3)
+        esp4 = findViewById(R.id.esp4)
+        esp5 = findViewById(R.id.esp5)
+        esp6 = findViewById(R.id.esp6)
+        esp7 = findViewById(R.id.esp7)
+        esp8 = findViewById(R.id.esp8)
+        esp9 = findViewById(R.id.esp9)
+
 
 
     }
