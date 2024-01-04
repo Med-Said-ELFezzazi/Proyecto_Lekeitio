9cafa8cd9a7e68a291f90defef3a078fddc3b8f9
Mohamed said El fezzazi <144590770+Med-Said-ELFezzazi@users.noreply.github.com>
2024-01-04 01:13:07 +0100

    ADD barra de progreso del audio

--- app/src/main/java/com/example/proyecto_lekeitio/Act3Activity.kt
+++ app/src/main/java/com/example/proyecto_lekeitio/Act3Activity.kt
@@ -1,26 +1,24 @@
 package com.example.proyecto_lekeitio
 
 import android.annotation.SuppressLint
-import android.content.Context
 import android.content.Intent
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.AttributeSet
-import android.util.Log
+import android.os.Handler
 import android.view.View
 import android.widget.Button
+import android.widget.SeekBar
 import android.widget.Toast
 import androidx.core.view.isVisible
-import androidx.fragment.app.FragmentTransaction
-import androidx.navigation.fragment.NavHostFragment.Companion.findNavController
-import androidx.navigation.fragment.findNavController
 
 class Act3Activity : AppCompatActivity() {
 
     private lateinit var mp: MediaPlayer
     private lateinit var btnPlayAudio: Button
     private lateinit var btnSiguienteVideo: Button
+    private lateinit var seekBarAudio: SeekBar
+    private val handler = Handler()
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,42 +28,71 @@ class Act3Activity : AppCompatActivity() {
         mp = MediaPlayer.create(this, R.raw.kaxarranka)
         btnPlayAudio = findViewById(R.id.btnPlayAudio)
         btnSiguienteVideo = findViewById(R.id.btnSiguienteVideo)
+        seekBarAudio = findViewById(R.id.seekBarAudio)
 
-        btnSiguienteVideo.isVisible = false //poner el boton invisible al principio
+        seekBarAudio.max = mp.duration
 
         btnPlayAudio.setOnClickListener {
             if (mp.isPlaying) {
-                // Si el audio está reproduciéndose, pausarlo
                 mp.pause()
+                handler.removeCallbacks(updateSeekBar)
                 Toast.makeText(this, "Pause", Toast.LENGTH_SHORT).show()
             } else {
-                // Si el audio no está reproduciéndose, iniciarlo o reanudarlo
                 mp.start()
+                handler.postDelayed(updateSeekBar, 0)
                 Toast.makeText(this, "Play", Toast.LENGTH_SHORT).show()
-
             }
-            // Actualizar la visibilidad del botón btnSiguienteVideo según el estado de reproducción
             btnSiguienteVideo.isVisible = true
+        }
+
+        seekBarAudio.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                if (fromUser) {
+                    mp.seekTo(progress)
+                }
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                // Opcional
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                // Opcional
+            }
+        })
+    }
+
+    private val updateSeekBar: Runnable = object : Runnable {
+        override fun run() {
+            if (mp.isPlaying) {
+                seekBarAudio.progress = mp.currentPosition
+                handler.postDelayed(this, 1000)
+            }
         }
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        // Liberar recursos del MediaPlayer al destruir la actividad (parar el audio)
+        if (mp.isPlaying) {
+            mp.stop()
+        }
         mp.release()
+        handler.removeCallbacks(updateSeekBar)
     }
-    fun playAudio(view: View) {
-        mp.start()
-        Toast.makeText(this,"Play",Toast.LENGTH_SHORT).show()
-    }
-
-
-    fun pasarAlVideo(view: View) {
-        //Depués de hacer click en siguiente , el audio debe parar
-        mp.release()
-        //Pasar a la siguiente pantalla
-        var intent = Intent(this,Act3Video::class.java)
-        startActivityForResult(intent, 5678)
-    }
-
 }
+
+/*
+fun playAudio(view: View) {
+    mp.start()
+    Toast.makeText(this,"Play",Toast.LENGTH_SHORT).show()
+}
+
+
+fun pasarAlVideo(view: View) {
+    //Depués de hacer click en siguiente , el audio debe parar
+    mp.release()
+    //Pasar a la siguiente pantalla
+    var intent = Intent(this,Act3Video::class.java)
+    startActivityForResult(intent, 5678)
+}
+*/
