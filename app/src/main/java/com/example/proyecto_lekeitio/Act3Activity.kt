7479230987fd7bec75700152af346a4630bb436e
Mohamed said El fezzazi <144590770+Med-Said-ELFezzazi@users.noreply.github.com>
2024-01-10 13:48:46 +0100

    FIX del problema de la foto mapa

--- app/src/main/java/com/example/proyecto_lekeitio/Act3Activity.kt
+++ app/src/main/java/com/example/proyecto_lekeitio/Act3Activity.kt
@@ -6,15 +6,15 @@ import android.content.Intent
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.util.AttributeSet
 import android.util.Log
 import android.view.View
 import android.widget.Button
-<<<<<<< Updated upstream
-=======
+
 import android.widget.ImageView
 import android.widget.SeekBar
->>>>>>> Stashed changes
+
 import android.widget.Toast
 import androidx.core.view.isVisible
 import androidx.fragment.app.FragmentTransaction
@@ -25,12 +25,12 @@ class Act3Activity : AppCompatActivity() {
 
     private lateinit var mp: MediaPlayer
     private lateinit var btnSiguienteVideo: Button
-<<<<<<< Updated upstream
-=======
+    private lateinit var btnPlayAudio: Button
+
     private lateinit var imgPlayAudio: ImageView
     private lateinit var seekBarAudio: SeekBar
     private val handler = Handler()
->>>>>>> Stashed changes
+
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,61 +38,76 @@ class Act3Activity : AppCompatActivity() {
         setContentView(R.layout.activity_act3)
 
         mp = MediaPlayer.create(this, R.raw.kaxarranka)
-<<<<<<< Updated upstream
+
         btnPlayAudio = findViewById(R.id.btnPlayAudio)
         btnSiguienteVideo = findViewById(R.id.btnSiguienteVideo)
 
-        btnSiguienteVideo.isVisible = false //poner el boton invisible al principio
-=======
-        //btnPlayAudio = findViewById(R.id.btnPlayAudio)
+        btnSiguienteVideo.isVisible = false //poner el bóton soguiente invisible al principio
+
         imgPlayAudio = findViewById(R.id.imgPlayAudio)
         seekBarAudio = findViewById(R.id.seekBarAudio)
         btnSiguienteVideo = findViewById(R.id.btnSiguienteVideo)
 
         seekBarAudio.max = mp.duration
         btnSiguienteVideo.isVisible = false
->>>>>>> Stashed changes
+
+
+        val updateSeekBar: Runnable = object : Runnable {
+            override fun run() {
+                if (mp.isPlaying) {
+                    seekBarAudio.progress = mp.currentPosition
+                    handler.postDelayed(this, 1000)
+                }
+            }
+        }
+
+
 
         imgPlayAudio.setOnClickListener {
             if (mp.isPlaying) {
                 // Si el audio está reproduciéndose, pausarlo
                 mp.pause()
-<<<<<<< Updated upstream
-=======
+
                 imgPlayAudio.setImageResource(R.drawable.play_debujo) // Cambiar a imagen de play
                 handler.removeCallbacks(updateSeekBar)
->>>>>>> Stashed changes
+
                 Toast.makeText(this, "Pause", Toast.LENGTH_SHORT).show()
             } else {
                 // Si el audio no está reproduciéndose, iniciarlo o reanudarlo
                 mp.start()
-<<<<<<< Updated upstream
-=======
+
                 imgPlayAudio.setImageResource(R.drawable.pause_debujo) // Cambiar a imagen de pause
                 handler.postDelayed(updateSeekBar, 0)
->>>>>>> Stashed changes
-                Toast.makeText(this, "Play", Toast.LENGTH_SHORT).show()
 
+                Toast.makeText(this, "Play", Toast.LENGTH_SHORT).show()
             }
             // Actualizar la visibilidad del botón btnSiguienteVideo según el estado de reproducción
             btnSiguienteVideo.isVisible = true
         }
+        seekBarAudio.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                if (fromUser) {
+                    mp.seekTo(progress)
+                }
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                // Opcional
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                // Opcional
+            }
+        })
+
     }
+
 
     override fun onDestroy() {
         super.onDestroy()
         // Liberar recursos del MediaPlayer al destruir la actividad (parar el audio)
         mp.release()
     }
-<<<<<<< Updated upstream
-    fun playAudio(view: View) {
-        mp.start()
-        Toast.makeText(this,"Play",Toast.LENGTH_SHORT).show()
-    }
-
-
-=======
-
 
     fun playAudio(view: View) {
         mp.start()
@@ -100,7 +115,6 @@ class Act3Activity : AppCompatActivity() {
     }
 
 
->>>>>>> Stashed changes
     fun pasarAlVideo(view: View) {
         //Depués de hacer click en siguiente , el audio debe parar
         mp.release()
@@ -108,8 +122,5 @@ class Act3Activity : AppCompatActivity() {
         var intent = Intent(this,Act3Video::class.java)
         startActivityForResult(intent, 5678)
     }
-<<<<<<< Updated upstream
 
-=======
->>>>>>> Stashed changes
 }
