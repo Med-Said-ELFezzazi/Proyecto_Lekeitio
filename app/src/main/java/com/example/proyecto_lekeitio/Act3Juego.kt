098e7151354fb0a22d3801d2f22481c170fe2a90
Mohamed said El fezzazi <144590770+Med-Said-ELFezzazi@users.noreply.github.com>
2024-01-14 20:39:37 +0100

    ADD audios de Motivación 'correcto/incorrecto'

--- app/src/main/java/com/example/proyecto_lekeitio/Act3Juego.kt
+++ app/src/main/java/com/example/proyecto_lekeitio/Act3Juego.kt
@@ -1,6 +1,7 @@
 package com.example.proyecto_lekeitio
 
 import android.annotation.SuppressLint
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
@@ -20,6 +21,12 @@ class Act3Juego : AppCompatActivity() {
 
     private lateinit var respuestaCorrecta: String
 
+    private lateinit var txtNumPregunta: TextView
+
+    //audios de respuestas
+    private lateinit var mpCorrecto: MediaPlayer
+    private lateinit var mpIncorrecto: MediaPlayer
+
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,12 +38,13 @@ class Act3Juego : AppCompatActivity() {
 
         cuadradoVacio = findViewById(R.id.cuadradoVacio)
 
+        txtNumPregunta = findViewById(R.id.txtNumPregunta)      //pa cambiar el número de pregunta
+
         // Establecer el texto predeterminado en el EditText
         val textoConFormato = """
         <html><body>
-        <h1 style="text-align:center; font-weight:bold;">Pregunta número 1</h1>
-        <p>¿Cuál es el instrumento utilizado?</p>
-        <p>(Elección correcta: silbido)</p>
+        <h1 style="text-align:center; font-weight:bold;">¿Cuál es el instrumento utilizado en el video?</h1>
+        <p style="text-align:center; color: blue;">(Selección: silbido)</p>
         </body></html>
          """.trimIndent()
 
@@ -47,17 +55,23 @@ class Act3Juego : AppCompatActivity() {
         cuadradoVacio.isClickable = false
         cuadradoVacio.isCursorVisible = false
 
-        respuestaCorrecta = "silbido"
+        respuestaCorrecta = "silbido"   //Inicialización (respuesta de pregunta1)
+
+        // Inicializa los objetos MediaPlayer
+        mpCorrecto = MediaPlayer.create(this, R.raw.yeah)
+        mpIncorrecto = MediaPlayer.create(this, R.raw.wrong_answer)
+
         btnSiguiente.setOnClickListener {
 //            respuestaCorrecta = "pandereta"
             if (respuestaCorrecta == "silbido") {
                 respuestaCorrecta = "pandereta"
             }
+            //Poner número de pregunto 2
+            txtNumPregunta.text = "Pregunta número 2"
             val textoConFormatoPregunta2 = """
             <html><body>
-            <h1 style="text-align:center; font-weight:bold;">Pregunta número 2</h1>
-            <p style="margin-left: 20px;">¿Cuál es el instrumento utilizado?</p>
-            <p style="margin-left: 20px;">(Elección correcta: pandereta)</p>
+            <h1 style="text-align:center; font-weight:bold;">¿Cuál es el instrumento utilizado en el video?</h1>
+            <p style="text-align:center; color: blue;">(Seleccióna: pandereta)</p>
             </body></html>
             """.trimIndent()
 
@@ -70,41 +84,82 @@ class Act3Juego : AppCompatActivity() {
         }
     }
 
+    /**
+     * Metodo que lanza el audio correcto
+     */
+    private fun lanzarAudioCorrecto() {
+        if (mpCorrecto.isPlaying) {
+            mpCorrecto.stop()
+            mpCorrecto.release()
+            mpCorrecto = MediaPlayer.create(this, R.raw.yeah)
+        }
+        mpCorrecto.start()
+    }
+
+    /**
+     * Metodo que lanza el audio Incorrecto
+     */
+    private fun lanzarAudioIncorrecto() {
+        if (mpIncorrecto.isPlaying) {
+            mpIncorrecto.stop()
+            mpIncorrecto.release()
+            mpIncorrecto = MediaPlayer.create(this, R.raw.wrong_answer)
+        }
+        mpIncorrecto.start()
+    }
+
     fun imagenSeleccionada(view: View) {
+        //resetBorders()      //quitar el borde colorado de la primera eleccion
         val imageView = view as ImageView
         when (view.id) {
             R.id.imagen1, R.id.imagen3 -> {
                 txtFotoSeleccionda.setText("Seleccion incorrecta!")
-                txtFotoSeleccionda.setTextColor(resources.getColor(R.color.rojo))
-                imageView.setBackgroundResource(R.drawable.borde_rojo)
+                txtFotoSeleccionda.setTextColor(resources.getColor(R.color.rojo)) //text
+                imageView.setBackgroundResource(R.drawable.borde_rojo)            //fondo
+                lanzarAudioIncorrecto()                                           //audio
             }
 
-            R.id.imagen2 -> {
-                if (respuestaCorrecta == "pandereta") {
-                    txtFotoSeleccionda.setText("Muy bien correcto")
-                    txtFotoSeleccionda.setTextColor(resources.getColor(R.color.verde))
-                    imageView.setBackgroundResource(R.drawable.borde_verde)
-                    btnSiguiente.isVisible = true
-                } else {
-                    txtFotoSeleccionda.setText("Seleccion incorrecta!")
-                    txtFotoSeleccionda.setTextColor(resources.getColor(R.color.rojo))
-                    imageView.setBackgroundResource(R.drawable.borde_rojo)
-                }
-            }
-
+            //Caso correcto de la pregunta 1
             R.id.imagen4 -> {
                 if (respuestaCorrecta == "silbido") {
                     txtFotoSeleccionda.setText("Muy bien correcto")
                     txtFotoSeleccionda.setTextColor(resources.getColor(R.color.verde))
                     imageView.setBackgroundResource(R.drawable.borde_verde)
                     btnSiguiente.isVisible = true
+                    lanzarAudioCorrecto()                 //audio
                 } else {
                     txtFotoSeleccionda.setText("Seleccion incorrecta!")
                     txtFotoSeleccionda.setTextColor(resources.getColor(R.color.rojo))
                     imageView.setBackgroundResource(R.drawable.borde_rojo)
+                    lanzarAudioIncorrecto()                 //audio
+                }
+            }
+
+            //Caso correcto de la pregunta 2
+            R.id.imagen2 -> {
+                if (respuestaCorrecta == "pandereta") {
+                    txtFotoSeleccionda.setText("Muy bien correcto")
+                    txtFotoSeleccionda.setTextColor(resources.getColor(R.color.verde))
+                    imageView.setBackgroundResource(R.drawable.borde_verde)
+                    btnSiguiente.isVisible = true
+                    lanzarAudioCorrecto()
+                } else {
+                    txtFotoSeleccionda.setText("Seleccion incorrecta!")
+                    txtFotoSeleccionda.setTextColor(resources.getColor(R.color.rojo))
+                    imageView.setBackgroundResource(R.drawable.borde_rojo)
+                    lanzarAudioIncorrecto()
                 }
             }
         }
     }
 
+    /**
+     * Metodo que quita el borde de respuesta (rojo/verde)
+     */
+    private fun resetBorders() {
+        val imageViews = listOf(R.id.imagen1, R.id.imagen2, R.id.imagen3, R.id.imagen4)
+        imageViews.forEach { id ->
+            findViewById<ImageView>(id).setBackgroundResource(0) // Quita cualquier borde
+        }
+    }
 }
