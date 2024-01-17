0a4f354c7563418571a71d7c6853669047e9cf51
Joseba <117350714+Josebasmh@users.noreply.github.com>
2024-01-17 13:15:36 +0100

    ADD-siguiente

--- app/src/main/java/com/example/proyecto_lekeitio/EnunAct5Activity.kt
+++ app/src/main/java/com/example/proyecto_lekeitio/EnunAct5Activity.kt
@@ -1,11 +1,19 @@
 package com.example.proyecto_lekeitio
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class EnunAct5Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_enun_act5)
+    }
+
+    fun siguiente(view: View) {
+        var intent = Intent(this, MapaActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
