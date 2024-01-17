996276a80110a018b1f348e46b583e228eb1f2ea
Joseba <117350714+Josebasmh@users.noreply.github.com>
2024-01-17 13:30:10 +0100

    Update VideoAct5Activity.kt

--- app/src/main/java/com/example/proyecto_lekeitio/VideoAct5Activity.kt
+++ app/src/main/java/com/example/proyecto_lekeitio/VideoAct5Activity.kt
@@ -1,11 +1,19 @@
 package com.example.proyecto_lekeitio
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class VideoAct5Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_video_act5)
+    }
+
+    fun siguiente(view: View) {
+        var intent = Intent(this, VideoAct5Activity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
