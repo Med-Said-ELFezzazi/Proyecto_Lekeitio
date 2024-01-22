84fdba7b6a5146a22dae70dd5a4758fbc27eab9d
HidalgoJoni <jonigarciah@gmail.com>
2024-01-22 09:54:06 +0100

    UPDATE Actividad 6 terminada

--- app/src/main/java/com/example/proyecto_lekeitio/Actividad6.kt
+++ app/src/main/java/com/example/proyecto_lekeitio/Actividad6.kt
@@ -4,33 +4,105 @@ import android.content.Intent
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.os.Handler
 import android.widget.Button
+import android.widget.SeekBar
 
 class Actividad6 : AppCompatActivity() {
     private lateinit var mediaPlayer: MediaPlayer
+    private lateinit var buttonPlay: Button
+    private lateinit var buttonPause: Button
+    private lateinit var buttonStop: Button
+    private lateinit var seekBar: SeekBar
     private lateinit var buttonNext: Button
+
+    private val handler = Handler()
+    private val updateSeekBar = object : Runnable {
+        override fun run() {
+            if (mediaPlayer.isPlaying) {
+                seekBar.progress = mediaPlayer.currentPosition
+                handler.postDelayed(this, 1000) // Actualiza cada segundo
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity6_principal)
 
-        buttonNext = findViewById(R.id.btnSopaDeLetras)
-        // Cambiar al nombre real del audio
-        mediaPlayer = MediaPlayer.create(this, R.raw.audio_maria_diaz_haro)
-    }
+        buttonPlay = findViewById(R.id.button_play_audio)
+        buttonPause = findViewById(R.id.button_pause_audio)
+        buttonStop = findViewById(R.id.button_stop_audio)
+        seekBar = findViewById(R.id.seekBar)
 
-    fun playAudio(view: View) {
-        mediaPlayer.start()
+        mediaPlayer = MediaPlayer.create(this, R.raw.maria_diaz_de_haro)
+
+        buttonPlay.setOnClickListener { playAudio() }
+        buttonPause.setOnClickListener { pauseAudio() }
+        buttonStop.setOnClickListener { stopAudio() }
+
+        // Inicializar SeekBar y gestionar los eventos de cambio
+        seekBar.max = mediaPlayer.duration
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                if (fromUser) {
+                    mediaPlayer.seekTo(progress)
+                }
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                if (mediaPlayer.isPlaying) {
+                    mediaPlayer.pause()
+                }
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                seekBar?.progress?.let { progress ->
+                    mediaPlayer.seekTo(progress)
+                    mediaPlayer.start()
+                }
+            }
+        })
+
+        buttonNext = findViewById(R.id.btnSopaDeLetras)
+        buttonNext.setOnClickListener { goToWordSearchActivity() }
+
+        // Deshabilitar el botón hasta que se cumpla alguna condición (opcional)
+        buttonNext.isEnabled = false
+
         mediaPlayer.setOnCompletionListener {
-            // Habilita el botón Siguiente una vez que el audio se haya reproducido completamente.
+            // Habilitar el botón Siguiente cuando el audio termine
             buttonNext.isEnabled = true
         }
     }
 
-    fun goToWordSearchActivity(view: View) {
+    private fun goToWordSearchActivity() {
         val intent = Intent(this, Actividad6SopaDeLetras::class.java)
         startActivity(intent)
+    }
+
+    private fun playAudio() {
+        if (!mediaPlayer.isPlaying) {
+            mediaPlayer.start()
+            handler.post(updateSeekBar) // Inicia la actualización del SeekBar
+        }
+    }
+
+    private fun pauseAudio() {
+        if (mediaPlayer.isPlaying) {
+            mediaPlayer.pause()
+            handler.removeCallbacks(updateSeekBar) // Detiene la actualización del SeekBar
+        }
+    }
+
+    private fun stopAudio() {
+        if (mediaPlayer.isPlaying) {
+            mediaPlayer.stop()
+            mediaPlayer.prepare()
+            handler.removeCallbacks(updateSeekBar) // Detiene la actualización del SeekBar
+        }
+        mediaPlayer.seekTo(0)
+        seekBar.progress = 0
     }
 
     override fun onDestroy() {
--- app/src/main/java/com/example/proyecto_lekeitio/Actividad6SopaDeLetras.kt
+++ app/src/main/java/com/example/proyecto_lekeitio/Actividad6SopaDeLetras.kt
@@ -1,5 +1,6 @@
 package com.example.proyecto_lekeitio
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.Gravity
 import android.widget.Button
@@ -8,17 +9,30 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
-class Actividad6SopaDeLetras : AppCompatActivity() { // Extiende AppCompatActivity
+class Actividad6SopaDeLetras : AppCompatActivity() {
     private lateinit var gridLayout: GridLayout
     private lateinit var buttonNext: Button
     private val gridSize = 5
     private val wordSearchLetters = arrayOf(
-        arrayOf('M', 'A', 'R', 'I', 'A'),
-        arrayOf('D', 'I', 'A', 'Z', 'D'),
-        arrayOf('E', 'H', 'A', 'R', 'O'),
-        arrayOf('L', 'P', 'Q', 'R', 'S'),
-        arrayOf('T', 'U', 'V', 'W', 'X')
+        arrayOf('M', 'A', 'R', 'I', 'A', 'R', 'G', 'R', 'I', 'B'),
+        arrayOf('E', 'D', 'L', 'E', 'T', 'Z', 'A', 'R', 'I', 'A'),
+        arrayOf('L', 'H', 'A', 'R', 'L', 'M', 'A', 'R', 'I', 'S'),
+        arrayOf('V', 'D', 'O', 'L', 'O', 'R', 'E', 'S', 'R', 'T'),
+        arrayOf('I', 'M', 'A', 'I', 'A', 'L', 'E', 'N', 'I', 'I'),
+        arrayOf('R', 'P', 'Q', 'R', 'S', 'M', 'A', 'R', 'I', 'D'),
+        arrayOf('A', 'U', 'V', 'B', 'I', 'C', 'E', 'N', 'T', 'A')
     )
+    private val words = listOf("MARIA", "ELVIRA", "DOLORES", "BASTIDA", "BICENTA", "MAIALEN")
+    private val wordsToClues = mutableMapOf(
+        "MARIA" to "..... de Maeztu",
+        "ELVIRA" to "..... Zipitria",
+        "DOLORES" to "..... Ibarruri",
+        "BASTIDA" to "Maria Dolores ..... Zalbidea",
+        "BICENTA" to "..... Moguel",
+        "MAIALEN" to "..... Lujanbio"
+    )
+    private lateinit var textViewClues: TextView
+
     private var selectedWord = StringBuilder()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,13 +43,42 @@ class Actividad6SopaDeLetras : AppCompatActivity() { // Extiende AppCompatActivi
         buttonNext = findViewById(R.id.button_next)
 
         setupWordSearchGrid()
+        textViewClues = findViewById(R.id.textViewClues)
+        updateCluesDisplay()
     }
 
+    private fun checkWord() {
+        if (selectedWord.toString() in wordsToClues.keys) {
+            // Actualiza la pista con la palabra encontrada
+            wordsToClues[selectedWord.toString()] = selectedWord.toString()
+
+            // Actualiza el TextView de pistas
+            updateCluesDisplay()
+
+            selectedWord.clear()
+            if (wordsToClues.keys.all { it in words }) {
+                buttonNext.isEnabled = true
+            }
+        }
+    }
+
+    private fun updateCluesDisplay() {
+        val cluesText = wordsToClues.values.joinToString("\n")
+        textViewClues.text = cluesText
+    }
+
+
     private fun setupWordSearchGrid() {
-        for (i in 0 until gridSize) {
-            for (j in 0 until gridSize) {
+        val rows = wordSearchLetters.size
+        val cols = wordSearchLetters[0].size
+
+        for (i in 0 until rows) {
+            for (j in 0 until cols) {
                 val textView = TextView(this)
-                textView.layoutParams = GridLayout.LayoutParams(GridLayout.spec(i, 1f), GridLayout.spec(j, 1f)).apply {
+                textView.layoutParams = GridLayout.LayoutParams(
+                    GridLayout.spec(i, 1f),
+                    GridLayout.spec(j, 1f)
+                ).apply {
                     width = 0
                     height = GridLayout.LayoutParams.WRAP_CONTENT
                     setMargins(4, 4, 4, 4)
@@ -43,6 +86,9 @@ class Actividad6SopaDeLetras : AppCompatActivity() { // Extiende AppCompatActivi
                 textView.gravity = Gravity.CENTER
                 textView.textSize = 24f
                 textView.text = wordSearchLetters[i][j].toString()
+                textView.isClickable = true
+                textView.isFocusable = true
+
                 textView.setOnClickListener {
                     handleLetterClick(i, j)
                 }
@@ -51,21 +97,23 @@ class Actividad6SopaDeLetras : AppCompatActivity() { // Extiende AppCompatActivi
         }
     }
 
-    private fun handleLetterClick(row: Int, col: Int) {
-        val letter = wordSearchLetters[row][col]
-        selectedWord.append(letter)
-        // Here you would handle the logic to check if the selected letters form a word
-        checkWord()
-    }
 
-    private fun checkWord() {
-        // Dummy check for the word 'MARIA'
-        if (selectedWord.toString() == "MARIA") {
-            Toast.makeText(this, "Palabra encontrada: $selectedWord", Toast.LENGTH_SHORT).show()
-            selectedWord.clear()
-            // Enable next button if all words are found
-            // For now, we just enable it after finding 'MARIA'
-            buttonNext.isEnabled = true
+    private fun handleLetterClick(row: Int, col: Int) {
+        val linealIndex = row * 10 + col
+        val textView = gridLayout.getChildAt(linealIndex) as TextView
+        val letter = wordSearchLetters[row][col]
+
+        // Cambiar el color de fondo o el texto para indicar selección
+        if (textView.isSelected) {
+            textView.isSelected = false
+            textView.setBackgroundColor(Color.TRANSPARENT) // o el color original
+            selectedWord.deleteCharAt(selectedWord.length - 1) // Elimina la última letra añadida
+        } else {
+            textView.isSelected = true
+            textView.setBackgroundColor(Color.YELLOW) // Cambia a un color para indicar selección
+            selectedWord.append(letter)
         }
+
+        checkWord()
     }
 }
