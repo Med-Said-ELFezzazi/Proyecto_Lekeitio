ad7df382b806a7de00a646b037dba084d82e88af
Joseba <117350714+Josebasmh@users.noreply.github.com>
2024-01-23 13:25:15 +0100

    ADD- porfin el puzzle

--- app/src/main/java/com/example/proyecto_lekeitio/Act1PuzzleActivity.kt
+++ app/src/main/java/com/example/proyecto_lekeitio/Act1PuzzleActivity.kt
@@ -1,17 +1,13 @@
 package com.example.proyecto_lekeitio
 
-import android.content.ClipData
-import android.content.res.Resources
 import android.graphics.drawable.Drawable
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.MotionEvent
 import android.view.View
-import android.view.View.OnDragListener
-import android.view.View.OnTouchListener
 import android.widget.Button
 import android.widget.ImageView
-import android.widget.LinearLayout
+import androidx.annotation.RequiresApi
 
 class Act1PuzzleActivity : AppCompatActivity() {
 
@@ -26,15 +22,8 @@ class Act1PuzzleActivity : AppCompatActivity() {
     private lateinit var img7 : ImageView
     private lateinit var img8 : ImageView
     private lateinit var img9 : ImageView
-    private lateinit var esp1 : LinearLayout
-    private lateinit var esp2 : LinearLayout
-    private lateinit var esp3 : LinearLayout
-    private lateinit var esp4 : LinearLayout
-    private lateinit var esp5 : LinearLayout
-    private lateinit var esp6 : LinearLayout
-    private lateinit var esp7 : LinearLayout
-    private lateinit var esp8 : LinearLayout
-    private lateinit var esp9 : LinearLayout
+    private var bSeleccionado : Boolean = false
+    private lateinit var iSeleccionado : ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,24 +31,6 @@ class Act1PuzzleActivity : AppCompatActivity() {
 
         // Implementación de variables
         btnMapa = findViewById(R.id.btnMapa)
-        img1 = findViewById(R.id.img1)
-        img2 = findViewById(R.id.img2)
-        img3 = findViewById(R.id.img3)
-        img4 = findViewById(R.id.img4)
-        img5 = findViewById(R.id.img5)
-        img6 = findViewById(R.id.img6)
-        img7 = findViewById(R.id.img7)
-        img8 = findViewById(R.id.img8)
-        img9 = findViewById(R.id.img9)
-        esp1 = findViewById(R.id.esp1)
-        esp2 = findViewById(R.id.esp2)
-        esp3 = findViewById(R.id.esp3)
-        esp4 = findViewById(R.id.esp4)
-        esp5 = findViewById(R.id.esp5)
-        esp6 = findViewById(R.id.esp6)
-        esp7 = findViewById(R.id.esp7)
-        esp8 = findViewById(R.id.esp8)
-        esp9 = findViewById(R.id.esp9)
 
         // Ocultar botón siguiente
         btnMapa.visibility = View.INVISIBLE
@@ -68,5 +39,103 @@ class Act1PuzzleActivity : AppCompatActivity() {
 
     fun siguiente(view: View) {
         finish()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.Q)
+    fun imagen1(view: View) {
+        img1 = findViewById(R.id.img1)
+        if (!bSeleccionado) {
+            seleccionarImagen(img1)
+        } else{
+            cambiarImagen(iSeleccionado,img1)
+        }
+    }
+
+    private fun seleccionarImagen(img: ImageView) {
+        iSeleccionado = img;
+        bSeleccionado = true
+        img.isEnabled= false
+    }
+
+
+    private fun cambiarImagen(imagen1: ImageView, imagen2: ImageView) {
+        var draSeleccionado = imagen1.drawable
+        imagen1.setImageDrawable(imagen2.drawable)
+        imagen2.setImageDrawable(draSeleccionado)
+        imagen1.isEnabled = true
+        bSeleccionado = false
+    }
+
+
+    fun imagen2(view: View) {
+        img2 = findViewById(R.id.img2)
+        if (!bSeleccionado) {
+            seleccionarImagen(img2)
+        } else{
+            cambiarImagen(iSeleccionado,img2)
+        }
+    }
+
+    fun imagen3(view: View) {
+        img3 = findViewById(R.id.img3)
+        if (!bSeleccionado) {
+            seleccionarImagen(img3)
+        } else{
+            cambiarImagen(iSeleccionado,img3)
+        }
+    }
+
+    fun imagen4(view: View) {
+        img4 = findViewById(R.id.img4)
+        if (!bSeleccionado) {
+            seleccionarImagen(img4)
+        } else{
+            cambiarImagen(iSeleccionado,img4)
+        }
+    }
+
+    fun imagen5(view: View) {
+        img5 = findViewById(R.id.img5)
+        if (!bSeleccionado) {
+            seleccionarImagen(img5)
+        } else{
+            cambiarImagen(iSeleccionado,img5)
+        }
+    }
+
+    fun imagen6(view: View) {
+        img6 = findViewById(R.id.img6)
+        if (!bSeleccionado) {
+            seleccionarImagen(img6)
+        } else{
+            cambiarImagen(iSeleccionado,img6)
+        }
+    }
+
+    fun imagen7(view: View) {
+        img7 = findViewById(R.id.img7)
+        if (!bSeleccionado) {
+            seleccionarImagen(img7)
+        } else{
+            cambiarImagen(iSeleccionado,img7)
+        }
+    }
+
+    fun imagen8(view: View) {
+        img8 = findViewById(R.id.img8)
+        if (!bSeleccionado) {
+            seleccionarImagen(img8)
+        } else{
+            cambiarImagen(iSeleccionado,img8)
+        }
+    }
+
+    fun imagen9(view: View) {
+        img9 = findViewById(R.id.img9)
+        if (!bSeleccionado) {
+            seleccionarImagen(img9)
+        } else{
+            cambiarImagen(iSeleccionado,img9)
+        }
     }
 }
