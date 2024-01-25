d1a7ee1a585ade7d672bae1f5e229e8bdc5b8fe3
Mohamed said El fezzazi <144590770+Med-Said-ELFezzazi@users.noreply.github.com>
2024-01-25 09:11:45 +0100

    UPDATE posicionamiento y funcionalidad del audio en la actividad3

--- app/src/main/java/com/example/proyecto_lekeitio/Act3Activity.kt
+++ app/src/main/java/com/example/proyecto_lekeitio/Act3Activity.kt
@@ -14,6 +14,7 @@ import android.widget.Button
 
 import android.widget.ImageView
 import android.widget.SeekBar
+import android.widget.TextView
 
 import android.widget.Toast
 import androidx.core.view.isVisible
@@ -25,7 +26,7 @@ class Act3Activity : AppCompatActivity() {
 
     private lateinit var mp: MediaPlayer
     private lateinit var btnSiguienteVideo: Button
-    private lateinit var btnPlayAudio: Button
+    //private lateinit var btnPlayAudio: Button
 
     private lateinit var imgPlayAudio: ImageView
     private lateinit var seekBarAudio: SeekBar
@@ -33,6 +34,8 @@ class Act3Activity : AppCompatActivity() {
 
     private var estabaPlayAntes: Boolean = false //Variable para controlar el estado del audio (play/pause)
 
+    private lateinit var txtTiempoActual: TextView      //textviews de la duracion de reproducción
+    private lateinit var txtTiempoTotal: TextView
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,24 +45,31 @@ class Act3Activity : AppCompatActivity() {
         mp = MediaPlayer.create(this, R.raw.kaxarranka)
 
         btnSiguienteVideo = findViewById(R.id.btnSiguienteVideo)
-        btnSiguienteVideo.isVisible = false //poner el bóton soguiente invisible al principio
 
         imgPlayAudio = findViewById(R.id.imgPlayAudio)
         seekBarAudio = findViewById(R.id.seekBarAudio)
 
+        txtTiempoActual = findViewById(R.id.currentTimeTextView)
+        txtTiempoTotal = findViewById(R.id.totalTimeTextView)
+
         seekBarAudio.max = mp.duration
 
+
         /**
-         * Actualiza el SeekBar para reflejar la posición actual del audio.
+         * Actualiza el SeekBar para reflejar la posición actual del audio
          */
+        txtTiempoTotal.text =  formateoTiempo(mp.duration)
         val updateSeekBar: Runnable = object : Runnable {
             override fun run() {
                 if (mp.isPlaying) {
                     seekBarAudio.progress = mp.currentPosition
+                    // actualizar el tiempo en el text view
+                    txtTiempoActual.text = formateoTiempo(mp.currentPosition)
                     handler.postDelayed(this, 1000)
                 }
             }
         }
+
 
 
         //Funcionamiento del bóton del audio
@@ -70,8 +80,7 @@ class Act3Activity : AppCompatActivity() {
 
                 imgPlayAudio.setImageResource(R.drawable.play_debujo) // Cambiar a imagen de play
                 handler.removeCallbacks(updateSeekBar)
-
-                Toast.makeText(this, "Pause", Toast.LENGTH_SHORT).show()
+                //Toast.makeText(this, "Pause", Toast.LENGTH_SHORT).show()
             } else {
                 // Si el audio no está reproduciéndose, iniciarlo o reanudarlo
                 mp.start()
@@ -79,10 +88,10 @@ class Act3Activity : AppCompatActivity() {
                 imgPlayAudio.setImageResource(R.drawable.pause_debujo) // Cambiar a imagen de pause
                 handler.postDelayed(updateSeekBar, 0)
 
-                Toast.makeText(this, "Play", Toast.LENGTH_SHORT).show()
+                //Toast.makeText(this, "Play", Toast.LENGTH_SHORT).show()
             }
             // Actualizar la visibilidad del botón btnSiguienteVideo según el estado de reproducción
-            btnSiguienteVideo.isVisible = true
+            //btnSiguiente.isVisible = true
         }
         seekBarAudio.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             //Cuando el progreso se cambia
@@ -110,24 +119,45 @@ class Act3Activity : AppCompatActivity() {
             }
         })
 
+        //cuando se reproduce el audio hasta el final
+        mp.setOnCompletionListener {
+            // Habilitar el botón Siguiente cuando el audio termine
+            imgPlayAudio.setImageResource(R.drawable.play_debujo)
+            btnSiguienteVideo.isVisible = true
+        }
+
+        btnSiguienteVideo.setOnClickListener{
+            pasarAlVideo()
+        }
     }
 
-
-    override fun onDestroy() {      //ESOS DOS METODOS CREO Q NO SON NECESARIOS LO QUE HACEN ES PLAY Y PAUSE EL AUDIO
-        super.onDestroy()
-        // Liberar recursos del MediaPlayer al destruir la actividad (parar el audio)
-        mp.release()
+    /**
+     * Metodo que ajusta el tiempo en minutos y segundos
+     */
+    fun formateoTiempo(milliseconds: Int): String {
+        val minutes = (milliseconds / 1000) / 60
+        val seconds = (milliseconds / 1000) % 60
+        return String.format("%d:%02d", minutes, seconds)
     }
 
-    fun pasarAlVideo(view: View) {
-        //Depués de hacer click en siguiente , el audio debe parar
-        mp.release()
-        //Pasar a la siguiente pantalla
-        var intent = Intent(this,Act3Video::class.java)
+    private fun pasarAlVideo() {
+        val intent = Intent(this, Act3Video::class.java)
         startActivity(intent)
-
-        //Acabar con esa pantalla
+        // Finalizar la actividad actual
         finish()
     }
 
+
+    override fun onPause() {
+        super.onPause()
+        if (mp.isPlaying) {
+            mp.pause() // Pausa el audio cuando la actividad está pausada
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mp.release() // Libera los recursos del MediaPlayer al destruir la actividad
+    }
+    
 }
