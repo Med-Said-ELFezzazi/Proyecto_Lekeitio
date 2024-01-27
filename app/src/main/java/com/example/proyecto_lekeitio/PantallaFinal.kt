01550d5a898e47796c2d01b5a6f6c13c394f97bf
Mohamed said El fezzazi <144590770+Med-Said-ELFezzazi@users.noreply.github.com>
2024-01-27 20:10:08 +0100

    UPDATE opacidad del fondo y tiempo de visibilidad del botón

--- app/src/main/java/com/example/proyecto_lekeitio/PantallaFinal.kt
+++ app/src/main/java/com/example/proyecto_lekeitio/PantallaFinal.kt
@@ -1,25 +1,36 @@
 package com.example.proyecto_lekeitio
 
 import android.media.MediaPlayer
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class PantallaFinal : AppCompatActivity() {
 
     private lateinit var mp: MediaPlayer
+    private lateinit var btnVolverMapa: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.gif_felicitaciones_final)
 
-        var btnVolverMapa : Button = findViewById(R.id.btnVolverMapa)
-        //Audio
+        btnVolverMapa = findViewById(R.id.btnVolverMapa)
+        btnVolverMapa.visibility = View.GONE  // El botón inicia como invisible
+
+        // Audio
         mp = MediaPlayer.create(this, R.raw.audio_felicitacion_final)
 
-        //Una vez cargada la pantalla , se lanza el audio
+        // Una vez cargada la pantalla, se lanza el audio
         mp.start()
 
-        btnVolverMapa.setOnClickListener{
+        // Hacer que el botón aparezca después de 3 segundos
+        Handler().postDelayed({
+            btnVolverMapa.visibility = View.VISIBLE
+        }, 3000)  // 3000 milisegundos = 3 segundos
+
+        btnVolverMapa.setOnClickListener {
             finish()
         }
     }
